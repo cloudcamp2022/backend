4517ada90da05bf2171ae1458fd1010f548eaad8
cloudcamp2022 <cloudcamp2022@gmail.com>
2022-10-27 15:48:17 +0900

    test3

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 from flask import Flask
 from flask_cors import CORS
 import pymysql
+import os
+
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
 @app.route("/hello")
 def hello():
-    host = "10.104.183.155"
-    user = "root"
-    password = "qwer1234"
-    db = "sjb_db"
+    host = os.environ['DB_HOST']
+    user = os.environ['DB_USER']
+    password = os.environ['MYSQL_ROOT_PASSWORD']
+    db = os.environ['DB_NAME']
     conn = pymysql.connect(host=host, user=user, db=db,
                            password=password, charset='utf8')
     curs = conn.cursor()
