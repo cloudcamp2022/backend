8fd66307f9d9286d18d093a5dd76b99124687e57
cloudcamp2022 <cloudcamp2022@gmail.com>
2022-10-27 15:35:45 +0900

    test2

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
 from flask import Flask
 from flask_cors import CORS
+import pymysql
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
 @app.route("/hello")
 def hello():
-    result = {"code": 200, "message": "jenkins test"}
+    host = "10.104.183.155"
+    user = "root"
+    password = "qwer1234"
+    db = "sjb_db"
+    conn = pymysql.connect(host=host, user=user, db=db,
+                           password=password, charset='utf8')
+    curs = conn.cursor()
+    sql = "select * from student";
+    curs.execute(sql)
+    rows = curs.fetchall()
+    print(rows)
+    conn.commit()
+    conn.close()
+
+    result = {"code": 200, "message": rows}
     return result
+
+
 
 
 if __name__ == "__main__":
