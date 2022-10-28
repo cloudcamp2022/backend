7b7494cb20cc3abeb35a06cff973477d41b8b467
cloudcamp2022 <cloudcamp2022@gmail.com>
2022-10-28 16:08:04 +0900

    test9

--- main.py
+++ main.py
@@ -2,10 +2,31 @@ from flask import Flask
 from flask_cors import CORS
 import pymysql
 import os
+import opentracing
+from flask_opentracing import FlaskTracing
 
+from flask import Flask, jsonify
+from jaeger_client import Config
+from flask_opentracing import FlaskTracer
+
+app = Flask(__name__)
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
+
+
+def initialize_tracer():
+    "Tracing setup method"
+    config = Config(
+        config={
+            'sampler': {'type': 'const', 'param': 1},
+        },
+        service_name='tasks-service')
+    return config.initialize_tracer()
+
+
+tracing = FlaskTracer(lambda: initialize_tracer(), True, app)
+
 
 @app.route("/hello")
 def hello():
@@ -25,8 +46,6 @@ def hello():
 
     result = {"code": 200, "message": rows}
     return result
-
-
 
 
 if __name__ == "__main__":
